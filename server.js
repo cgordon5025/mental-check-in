1868ae23bdebb9f147e8a350ea3531f0eb24e9f9
Charlee Gordon <cgordon5025@gmail.com>
2022-11-19 17:29:44 -0500

    initial push

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const Models = require('./models')
+const seedAll = reuiqre('./seeds/index')
+const exphb = require('express-handlebars')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const path = require('path')
+const { strict } = require('assert')
+
+const routes = require('./controllers')
+const sequelize = require('./config/connection')
+const helpers = require('./utils/helpers')
+const { setDefaultResultOrder } = require('dns');
+const exp = require('constants');
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+const sess = {
+    secret: "super duper secret",
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+}
+
+app.use(session(sess))
+
+const hbs = exphb.create({ helpers })
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.listen(routes)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: true }).then(async () => {
+    await seedAll()
+    await app.listen(PORT, () => {
+        console.log(`App Listening on port ${PORT}`)
+    });
+})
+
